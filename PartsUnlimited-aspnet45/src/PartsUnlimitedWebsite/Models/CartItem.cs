65ff9ab1cf676ca0c98782cce839524bbd41cfed
sapphire1996 <adepelumi1996@gmail.com>
2020-01-24 12:43:00 -0800

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
-    //my first change..
+    //my first change
     public class CartItem : ILineItem
     {
         [Key]
